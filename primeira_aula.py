e176b4dfe2d97c59a25f9e25f7e2fe3a4067b6bc
michelebazana <michele.bazana@gmail.com>
2025-03-17 20:52:04 -0300

    Meu primeiro programa em Python.

--- primeira_aula.py
+++ primeira_aula.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+#print("Hello World!")
+'''print("Hello World!")'''
+
+check1 = 9
+check2 = 7.5
+check3 = 8
+
+media = (check1+check2+check3)/3
+print(media)
+#print((check1+check2+check3)/3)
+print(f"Média = {media:.2f}")
