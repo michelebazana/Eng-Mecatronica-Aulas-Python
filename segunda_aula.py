ec1849d7702dd2d148b8654f346d4d6fdbd3c398
michelebazana <michele.bazana@gmail.com>
2025-03-24 21:00:08 -0300

    Segunda uala de Python.

--- segunda_aula.py
+++ segunda_aula.py
@@ -7,9 +7,32 @@
 # print(2*a*3*b)
 
 # Exercício 3
-num1 = 1
-num2 = 2
-num3 = 3.5
-soma = num1+num2+num3
-print(soma)
+# num1 = 1
+# num2 = 2
+# num3 = 2.16666666
+# soma = num1+num2+num3
+# print(soma)
+#
+# print(type(num1))
+# print(type(num3))
+#
+# print(f"Soma = {soma:.2f}")
+
+# d = True
+# print(type(d))
+#
+# nome = "Michele"
+# print(type(nome))
+
+# print(10%3)
+# print(10//3)
+# print(10**3)
+
+nome_comp = input("Nome completo: ")
+idade = int(input("Idade: "))
+altura = float(input("Altura: "))
+print(nome_comp)
+print(idade)
+print(altura)
+
 
