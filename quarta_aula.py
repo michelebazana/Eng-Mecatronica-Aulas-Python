f9df78ad20be2c6a7c0905e80c3bc31d0070e33d
michelebazana <Sato2024@>
2025-04-28 21:00:21 -0300

    Aula sobre while

---
+++ quarta_aula.py
@@ -0,0 +1,28 @@
+# print(1)
+# print(2)
+# print(3)
+
+# x = 1
+# print(x)
+# x = 2
+# print(x)
+
+# x = 1
+# print(x)
+# x = x + 1
+# print(x)
+
+# x = 0
+# while x <= 3:
+#     print(x)
+#     # x = x + 1
+#     x += 1
+
+from time import sleep
+x = 10 # contador
+while x >= 0: # condição
+    print(x)
+    sleep(1)
+    x -= 1
+
+print("Fogo!")
