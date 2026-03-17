7361e98b63b00fc16f3c1583ad73b0e55393f28b
michelebazana <michele.bazana@gmail.com>
2026-03-16 22:46:53 -0300

    Terceira aula

--- terceira_aula.py
+++ terceira_aula.py
@@ -1,38 +1,23 @@
-# media_final = float(input("Média final de 0 a 10: "))
-#
+# media_final = float(input("Média final = "))
 # if media_final >= 6:
-#     print("Aprovado!")
+#     print("Aprovado")
 # else:
-#     print("Reprovado!")
+#     print("Reprovado")
 
 # Exercício 1
-# velocidade = float(input("Velocidade do carro: "))
-# if velocidade > 80:
-#     print("Você foi multado!")
-#     multa = (velocidade - 80) * 5
-#     print(f"O valor da multa é R$ {multa:.2f}")
+# vel = int(input("Velocidade em km = "))
+# if vel > 80:
+#     print("Você foi multado.")
+#     multa = (vel - 80) * 5
+#     print(f"O valor da multa é R${multa:.2f}")
 # else:
-#     print("Parabéns! Você está dentro velocidade permitida!")
+#     print("Você não foi multado")
 
 # Exercício 2
-num1 = float(input("Número 1: "))
-num2 = float(input("Número 2: "))
-num3 = float(input("Número 3: "))
-
-if num1 >= num2 and num1 >= num3:
-    print(f"O maior número é {num1}")
-elif num2 >= num1 and num2 >= num3:
-    print(f"O maior número é {num2}")
-elif num3 >= num1 and num3 >= num2:
-    print(f"O maior número é {num3}")
-
-if num1 <= num2 and num1 <= num3:
-    print(f"O menor número é {num1}")
-elif num2 <= num1 and num2 <= num3:
-    print(f"O menor número é {num2}")
-elif num3 <= num1 and num3 <= num2:
-    print(f"O menor número é {num3}")
-
+# num1 = float(input("Número 1 = "))
+# num2 = float(input("Número 2 = "))
+# num3 = float(input("Número 3 = "))
+#
 # maior = num1
 # if num2 >= num1 and num2 >= num3:
 #     maior = num2
@@ -45,5 +30,22 @@ elif num3 <= num1 and num3 <= num2:
 # if num3 <= num1 and num3 <= num2:
 #     menor = num3
 #
-# print(maior)
-# print(menor)
+# print(f"O número maior é {maior}")
+# print(f"O número menor é {menor}")
+
+# Exercício 3
+# sal = float(input("Salário = R$ "))
+# if sal > 1250:
+#     aumento = sal * 0.1
+# else:
+#     aumento = sal * 0.15
+#     print(aumento)
+# print(f"O aumento é de R${aumento:.2f}")
+
+# Exercício 4
+dis = int(input("Distância em km = "))
+if dis <= 200:
+    preco = dis * 0.5
+else:
+    preco = dis * 0.45
+print(f"O valor da passagem é R${preco:.2f}")
